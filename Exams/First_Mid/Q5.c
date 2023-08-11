ca2928022057b30583a14a20e61f22f6fe397262
Mohamed Demrdash <mohameddemrdash741@gmail.com>
2023-08-11 12:06:12 +0300

    number of ones between two zersos

---
+++ Exams/First_Mid/Q5.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+
+void num_ones(int num);
+
+void main ()
+{
+	int num = 0 ;
+	scanf("%d",&num);
+	num_ones(num);
+}
+
+
+void num_ones(int num)
+{
+	int one = 0 ;
+	while(num)
+	{
+		if(num &1 )
+		{
+			one++;
+		}
+		else
+		{
+			
+		}
+		num >>=1;
+	}
+	printf("%d",one);
+}
