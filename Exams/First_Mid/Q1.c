c9dd6bc9aafad74f5dcfc15898729a000fa66e3b
Mohamed Demrdash <mohameddemrdash741@gmail.com>
2023-08-11 15:10:49 +0300

    sum digits of the number

---
+++ Exams/First_Mid/Q1.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+
+void sum_dig(int num);
+
+void main ()
+{
+	int num = 0 ;
+	scanf("%d",&num);
+	sum_dig(num);
+}
+
+
+void sum_dig(int num)
+{
+	int sum =0,dig =1;
+	int flag = num;
+	while(flag /= 10)
+	{
+		dig++;
+	}
+	while(dig--)
+	{
+		sum += num%10;
+		num /= 10;
+	}
+	printf("%d",sum);
+}
