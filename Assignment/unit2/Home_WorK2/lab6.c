9f7a57677d1154a2a2eb14bb67e2d09af2cb39bd
Mohamed Demrdash <mohameddemrdash741@gmail.com>
2023-08-11 16:27:43 +0300

    sum of the natural numbers

---
+++ Assignment/unit2/Home_WorK2/lab6.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+
+
+
+void main ()
+{
+	int num ;
+	printf("enter an integer : ");
+	scanf("%d",&num);
+	printf("sum = %ld",num*(num+1)/2);
+}
+
+
