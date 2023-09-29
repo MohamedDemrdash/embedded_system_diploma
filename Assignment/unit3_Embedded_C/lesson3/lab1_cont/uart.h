feb86476d4abea356d1ea81a3caaa897f977e4ef
Mohamed Demrdash <mohameddemrdash741@gmail.com>
2023-09-30 00:44:21 +0300

    using make file

---
+++ Assignment/unit3_Embedded_C/lesson3/lab1_cont/uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ * Created on: Aug 17, 2023
+ * Author: Mohamed Hussein Demrdash
+ * 
+ */
+
+#ifndef _UART_H_
+#define _UART_H_
+
+#include "platform_types.h"
+
+void uart_send_string(const uint8 * P_tx_string);
+
+#endif
